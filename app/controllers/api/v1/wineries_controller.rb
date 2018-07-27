3f79b7296ebb18cae5dfd9f980b72e854b74ff45
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-27 15:31:27 -0400

    add api request

--- app/controllers/api/v1/wineries_controller.rb
+++ app/controllers/api/v1/wineries_controller.rb
@@ -5,6 +5,12 @@ class Api::V1::WineriesController < ApplicationController
   end
 
   def show
+    URL="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{params["search"]}&inputtype=textquery&fields=photos,id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
+    
+    @winery = RestClient.get(URL)
+    @winery = JSON.parse(@winery)
+        
+    render json: @winery
   end
 
 end
