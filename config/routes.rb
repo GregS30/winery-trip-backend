4f97ff2c24190dda07a950c0541791ecc9e35148
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-28 21:07:58 -0400

    uptodate with last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # resources :regions
-  # resources :trip_wineries
-  # resources :wines
-  # resources :wineries
-  # resources :trips
-  # resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       resources :wines, only: [:index]
