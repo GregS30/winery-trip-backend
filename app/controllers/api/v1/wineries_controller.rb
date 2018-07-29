19abdcba4f8dea1a8595ae15b76ef30ee03f3e63
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-29 03:52:25 -0400

    add google places detail json repsonse

--- app/controllers/api/v1/wineries_controller.rb
+++ app/controllers/api/v1/wineries_controller.rb
@@ -5,12 +5,21 @@ class Api::V1::WineriesController < ApplicationController
   end
 
   def return_winery_api_results
-    url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{params["search"]}&inputtype=textquery&fields=photos,id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
-    
+    url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{params["search"]}&inputtype=textquery&fields=photos,id,place_id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
+
     @winery = RestClient.get(url)
+
     @winery = JSON.parse(@winery)
-        
-    render json: @winery
+
+    @wineryid = @winery["candidates"][0]["place_id"]
+
+    detailurl="https://maps.googleapis.com/maps/api/place/details/json?placeid=#{@wineryid}&fields=photos,id,place_id,icon,types,formatted_address,price_level,formatted_phone_number,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
+
+    @winerydetail = RestClient.get(detailurl)
+
+    @winerydetail = JSON.parse(@winerydetail)
+
+    render json: @winerydetail
   end
 
 end
