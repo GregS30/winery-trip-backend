ddfe2a3215b2b204f8d0456d04a5163f81cbf12e
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-30 16:52:05 -0400

    add route w/ search logic

--- app/controllers/api/v1/wineries_controller.rb
+++ app/controllers/api/v1/wineries_controller.rb
@@ -1,12 +1,53 @@
 class Api::V1::WineriesController < ApplicationController
-  def index
-    @wineries = Winery.all
-    render json: @wineries
-  end
 
-  def index_filter
-    
-  end
+    def index
+
+      select_sql = " wineries y"
+      join_sql = ""
+      filter_sql = ""
+  
+      # replace with params
+      grape = params["grape"]
+      region = params["region"]
+  
+      # default region to Napa Valley
+      if !grape and !region
+        region = "Napa Valley"
+      end
+  
+      puts("region: #{region}")
+      puts("grape: #{grape}")
+  
+      # build sql needed to query for a grape (varietal)
+      if grape
+        select_sql = select_sql + ", wines w, grapes g"
+        join_sql = join_sql + " y.id = w.winery_id and g.id = w.grape_id"
+        filter_sql = filter_sql + " and g.name='#{grape}'"
+      end
+  
+      # build sql needed to query for a region
+      if region
+        select_sql = select_sql + ", regions r"
+        if grape
+          join_sql = join_sql + " and"
+        end
+        join_sql = join_sql + " r.id = y.region_id"
+        filter_sql = filter_sql + " and r.name='#{region}'"
+      end
+  
+      # put it all together
+      sql = "select distinct y.* from" + select_sql
+      if grape || region
+        sql = sql + " where " + join_sql + filter_sql
+      end
+  
+      puts("sql: #{sql}")
+  
+      #    @wineries = Winery.all
+      @wineries = Winery.find_by_sql(sql)
+  
+      render json: @wineries
+    end
 
   def return_winery_api_results
     url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{URI.encode_www_form_component(params["search"])}&inputtype=textquery&fields=photos,id,place_id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
