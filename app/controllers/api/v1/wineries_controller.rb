dfe6645bd5d7e6d257bfd40268258b9efcc21009
Alberto Carreras <a.carreras.c@gmail.com>
2018-07-30 12:36:04 -0400

    fix null/conflictive api searches

--- app/controllers/api/v1/wineries_controller.rb
+++ app/controllers/api/v1/wineries_controller.rb
@@ -9,21 +9,26 @@ class Api::V1::WineriesController < ApplicationController
   end
 
   def return_winery_api_results
-    url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{params["search"]}&inputtype=textquery&fields=photos,id,place_id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
+    url="https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{URI.encode_www_form_component(params["search"])}&inputtype=textquery&fields=photos,id,place_id,icon,types,formatted_address,price_level,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
 
-    @winery = RestClient.get(url)
+    @winery = RestClient.get(url) 
 
     @winery = JSON.parse(@winery)
 
-    @wineryid = @winery["candidates"][0]["place_id"]
+    if @winery["candidates"] != []
 
-    detailurl="https://maps.googleapis.com/maps/api/place/details/json?placeid=#{@wineryid}&fields=photos,id,place_id,icon,types,formatted_address,price_level,formatted_phone_number,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
+        @wineryid = @winery["candidates"][0]["place_id"]
 
-    @winerydetail = RestClient.get(detailurl)
+        detailurl="https://maps.googleapis.com/maps/api/place/details/json?placeid=#{@wineryid}&fields=photos,id,place_id,icon,types,formatted_address,price_level,formatted_phone_number,name,rating,opening_hours,id,plus_code,geometry&key=#{ENV['GOOGLE_PLACES_API']}"
 
-    @winerydetail = JSON.parse(@winerydetail)
+        @winerydetail = RestClient.get(detailurl)
 
-    render json: @winerydetail["result"]
+        @winerydetail = JSON.parse(@winerydetail)
+        
+        render json: @winerydetail["result"]
+    else 
+      render json: {message: "No Data"}
+    end
   end
 
 end
