99c7386f808536556203d7450dcc0917317ad086
GregS30 <gjschmitt@live.com>
2018-08-03 09:39:25 -0400

    refactor phase 1 completed

--- app/controllers/api/v1/wines_controller.rb
+++ app/controllers/api/v1/wines_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::WinesController < ApplicationController
 
   def index
-    @winesFound = Wine.all.select { |wine| wine.winery_id == params["wineryId"].to_i }
+    # @winesFound = Wine.all.select { |wine| wine.winery_id == params["wineryId"].to_i }
+
+    sql = "select * from wines where winery_id=#{params["wineryId"].to_i}"
+
+    puts("sql: #{sql}")
+
+    @winesFound = Wine.find_by_sql(sql)
+
     render json: @winesFound
   end
 
